1ca5236a4a4b76507f03587a750ac25846ffaa8f
hcalldee <hcalldee@gmail.com>
2024-02-20 09:40:06 +0800

    perbaikan perbaikan config token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    telegramBotToken: '6486283892:AAGg_Sxn8HFXBBKG6NGLMFZVAxELzqrQABs',
+    telegramBotToken: '',
     databaseConfig: {
       host: '192.168.1.4',
       user: 'root',
