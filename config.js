be1fe93cd1b4173e4537a1095dbc4acab7fa8e91
hcalldee <hcalldee@gmail.com>
2024-02-22 13:37:35 +0800

    pnambahan access token

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    telegramBotToken: '',
+    githubAccessToken: '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',
+    telegramBotToken: '6486283892:AAGg_Sxn8HFXBBKG6NGLMFZVAxELzqrQABs',
     databaseConfig: {
       host: '192.168.1.4',
       user: 'root',
